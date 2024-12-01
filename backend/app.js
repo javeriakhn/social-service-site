e812b5f08adfb9bb7806f410677f89e43cb06570
javeriakhn <137407291+javeriakhn@users.noreply.github.com>
2024-12-01 18:32:28 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
